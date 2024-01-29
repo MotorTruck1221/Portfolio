e190522528f0bf1ba873d3620f2301c807f0c64c
MotorTruck1221 <tuckerj0606@icloud.com>
2024-01-28 21:56:07 -0700

    update

--- components/Bar.vue
+++ components/Bar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="hidden md:flex flex-col h-full gap-4 justify-center items-center">
-        <div class="h-1/3 border-2 border-cat-pink w-1"></div>
-        <NuxtLink to="https://github.com/motortruck1221/" target="_blank" class="i-ri-github-fill text-3xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
-        <NuxtLink to="https://discord.com/users/818995901791207454" target="_blank" class="i-ri-discord-fill text-3xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
-        <NuxtLink to="https://twitter.com/motortruck1221" target="_blank" class="i-ri-twitter-fill text-3xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
-        <div class="h-1/3 border-2 border-cat-pink w-1"></div>
+        <div class="h-1/2 border-2 border-cat-pink w-1"></div>
+        <NuxtLink to="https://github.com/motortruck1221/" target="_blank" class="i-ri-github-fill text-5xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
+        <NuxtLink to="https://discord.com/users/818995901791207454" target="_blank" class="i-ri-discord-fill text-5xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
+        <NuxtLink to="https://twitter.com/motortruck1221" target="_blank" class="i-ri-twitter-fill text-5xl transition-all ease-in-out duration-300 hover:text-cat-flamingo"></NuxtLink>
+        <div class="h-1/2 border-2 border-cat-pink w-1"></div>
     </div>
 </template>
