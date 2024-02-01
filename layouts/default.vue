930a535f6d248a337e3ff5a4a9aab4376275975c
MotorTruck1221 <tuckerj0606@icloud.com>
2024-01-31 23:23:10 -0700

    Icon, some meta tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,17 @@
     import '~/assets/overwrites.scss';
     useHead({
         titleTemplate: (name) => {return name ? `${name} - MotorTruck1221` : 'MotorTruck1221'},
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: 'MotorTruck1221\'s personal website. And Blog.'},
+            {name: 'format-detection', content: 'telephone=no'},
+            {name: 'msapplication-TileColor', content: '#000000'},
+            {name: 'theme-color', content: '#000000'},
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-png', href: '/logo.png'},
+        ],
     })
     const needBg = ref(true);
     const particlesLoaded = (container?: Container) => {
