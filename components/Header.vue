aeef7c787569c19724f674f4fa5b5a8676234582
MotorTruck1221 <tuckerj0606@icloud.com>
2024-02-03 13:24:47 -0700

    Mobile responsive header

--- components/Header.vue
+++ components/Header.vue
@@ -3,11 +3,11 @@
 </script>
 <template>
     <div class="bg-cat-crust p-2 h-12 text-cat-text flex flex-row justify-between items-center">
-        <NuxtLink to="/" class="text-2xl font-bold">MotorTruck1221</NuxtLink>
-        <div class="hidden md:flex flex-row justify-right items-center gap-4">
-            <NuxtLink to="/" :class="route.path === '/' ? 'text-cat-pink' : ''" class="text-lg hover:text-cat-sapphire font-semibold">Home</NuxtLink>
-            <NuxtLink to="/projects" :class="route.path === '/projects' ? 'text-cat-pink' : ''" class="text-lg hover:text-cat-sapphire font-semibold">Projects</NuxtLink>
-            <NuxtLink to="/blog" :class="route.path === `/blog/${route.params.slug}` ? 'text-cat-pink' : '' || route.path === '/blog' ? 'text-cat-pink' : ''" class="text-lg mr-2 hover:text-cat-sapphire font-semibold">Blog</NuxtLink>
+        <NuxtLink to="/" class="hidden md:block text-2xl font-bold">MotorTruck1221</NuxtLink>
+        <div class="flex flex-row justify-right items-center gap-2 md:gap-4">
+            <NuxtLink to="/" :class="route.path === '/' ? 'text-cat-pink' : ''" class="text-md md:text-lg hover:text-cat-sapphire font-semibold">Home</NuxtLink>
+            <NuxtLink to="/projects" :class="route.path === '/projects' ? 'text-cat-pink' : ''" class="text-md md:text-lg hover:text-cat-sapphire font-semibold">Projects</NuxtLink>
+            <NuxtLink to="/blog" :class="route.path === `/blog/${route.params.slug}` ? 'text-cat-pink' : '' || route.path === '/blog' ? 'text-cat-pink' : ''" class="text-md md:text-lg mr-2 hover:text-cat-sapphire font-semibold">Blog</NuxtLink>
         </div>
     </div>
 </template>
