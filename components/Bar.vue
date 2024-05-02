185a64e8d8c12a15bb43d8fb7cfde335de9cc848
MotorTruck1221 <motortruck1221@protonmail.com>
2024-05-02 09:01:43 -0600

    Fix contrast for blog stuff

--- components/Bar.vue
+++ components/Bar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hidden md:flex flex-col h-full gap-4 justify-center items-center">
+    <div class="hidden md:flex flex-col h-full gap-4 justify-center items-center sticky">
         <div class="h-1/2 border-2 border-cat-pink w-1"></div>
         <NuxtLink to="https://github.com/motortruck1221/" target="_blank" class="w-20 h-20 flex justify-center items-center text-4xl transition-all ease-in-out duration-300 hover:text-cat-flamingo">
             <font-awesome-icon :icon="['fab', 'github']" />
