9fe7703f4b82a923899f695984f7ec6bd7bde31d
MotorTruck1221 <motortruck1221@protonmail.com>
2024-05-02 12:38:42 -0600

    More styling BS

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -5,8 +5,7 @@
     <div class="text-cat-text p-4 w-full h-full flex flex-row justify-left items-top">
         <div class="h-full w-full flex flex-col justify-center items-center">
             <div class="flex flex-col justify-left item-top h-full w-full bg-cat-crust opacity-90 p-4 rounded-lg 
-                prose prose-slate prose-invert prose-md
-                overflow-y-auto overflow-x-hidden text-wrap break-words text-white">
+                prose prose-slate prose-invert prose-md overflow-y-auto overflow-x-hidden text-wrap break-words text-white">
                 <ContentDoc />
                 <div class="h-16 w-full flex flex-row justify-between items-center gap-4 mt-auto">
                     <NuxtLink to="/blog">Back</NuxtLink>
